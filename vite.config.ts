73f6a76fff83aadb183549db4493ed5f113b4bcb
Tatsiana-prog <programvtv07@gmail.com>
2025-04-26 16:17:55 +0300

    fix:corrected routes for images

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/yee-daa/',  // имя репозитория или подпапки на GitHub Pages
-  });
+  plugins: [react()],
+  base: '/yee-daa/',
+});
